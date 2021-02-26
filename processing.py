ae5c491c20a3516df272b359161c653712cfc1e4
Powerbyte7 <joranklaui@gmail.com>
2021-02-26 10:31:30 +0100

    Fix mesh output folder creation

--- processing.py
+++ processing.py
@@ -18,14 +18,27 @@ class SEURAT_OT_process_data(bpy.types.Operator):
             self.report({'ERROR'}, 'Processing Seurat data only possible on Windows')
             return {'CANCELLED'}
 
+        output_directory = bpy.path.abspath(opt.mesh_output_path)
+
         input_path = bpy.path.abspath(opt.capture_output_path) + "manifest.json"
         output_path = bpy.path.abspath(opt.mesh_output_path) + "output"
+
+        if not os.path.exists(output_directory):
+            try:
+                os.mkdir(output_directory)
+            except:
+                self.report({'ERROR'}, 'Failed to create output folder, check the mesh output path')
+                return {'CANCELLED'}
+            
+            print ("Created output directory")
+        else:
+            print ("Output directory exists")
 
         script_file = os.path.realpath(__file__)
         directory = os.path.dirname(script_file)
 
         options = opt.seurat_command_flags
-        # "-texture_width 8192 -texture_height 8192 -pixels_per_degree 20 -triangle_count 180000"
+        # Default is "-texture_width 8192 -texture_height 8192 -pixels_per_degree 20 -triangle_count 180000"
 
         cmd = directory + "\seurat-pipeline-msvc2017-x64.exe -input_path " + input_path + " -output_path " + output_path + " " + options
 
